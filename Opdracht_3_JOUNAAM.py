ca6f7dd0324da9d67588b558d0b107bcb5d39954
ruth98765 <146540972+ruth98765@users.noreply.github.com>
2023-11-15 10:47:20 +0100

    Add files via upload

---
+++ Opdracht_3_JOUNAAM.py
@@ -0,0 +1,71 @@
+# Oefening 1: Vierkant
+'''Schrijf een Python-programma dat de gebruiker vraagt om de lengte en breedte van een rechthoek in te voeren.
+    Bereken vervolgens de omtrek en oppervlakte van de rechthoek en druk deze waarden af.'''
+'''
+Kvadrāta laukums S=a⋅a jeb S=a2.
+Kvadrāta perimetrs P=4a,
+Šeit S ir laukums, P ir perimetrs, a ir kvadrāta mala.
+ 
+Taisnstūra laukums S=a⋅b.
+Taisnstūra perimetrs P=2a+2b jeb P=2(a+b).
+Šeit S ir laukums, P ir perimetrs, a un b taisnstūra garums un platums.
+ 
+Kuba tilpums V=a3, kur V ir tilpums un a ir kuba šķautnes garums.
+ 
+Taisnstūra paralēlskaldņa tilpums V=a⋅b⋅c, kur V ir tilpums un a,b,c ir garums, platums un augstums.
+ '''
+# Example:
+'''
+Voer de lengte van de rechthoek in: 46
+Voer de breedte van de rechthoek in: 8
+De omtrek van de rechthoek is:  108.0
+De oppervlakte van de rechthoek is:  368.0
+'''
+
+# Oefening 2: Celsius naar Fahrenheit
+'''Schrijf een programma dat de gebruiker vraagt om een temperatuur in Celsius in te voeren en deze omzet naar 
+Fahrenheit met behulp van de formule: Fahrenheit = (Celsius * 9/5) + 32.'''
+
+# Example:
+'''Geef graden in celsius in: 20
+Omgerekend in dit  68.0 graden Fahrenheit'''
+
+# Oefening 3: Nummers vergelijken
+'''Schrijf een programma dat drie getallen van de gebruiker vraagt en deze te vergelijken met elkaar.'''
+
+# Example:
+'''
+Geef een getal in:5
+Geef een tweede getal in:8
+Geef een derde getal in:5
+Getal 1 is groter of gelijk aan getal 3: True
+Getal 2 is kleiner dan getal 3: False
+Getal 2 is niet gelijk aan getal 1: True
+Het grootste getal van de drie is:  8
+Het kleinste getal van de drie is:  5
+'''
+
+# Oefening 4: Positief, negatief of nul
+'''Schrijf een programma dat de gebruiker vraagt om een getal in te voeren en vervolgens controleert of het getal 
+positief, negatief of nul is.'''
+
+# Example:
+'''
+Geef een getal in:-6
+Het getal is positief:  False
+Het getal is 0:  False
+Het getal is negatief:  True
+'''
+
+# Oefening 5: Cirkel
+'''Schrijf een programma dat de omtrek en het oppervlakte van een cirkel berekent. De gebruiker moet de straal van de 
+cirkel als input invoeren. Gebruik voor pi 3.14159. Zorg voor een gepaste variabele.
+Riņķa laukums S=πR^2'''
+
+# Example:
+'''
+Geef de straal in van een cirkel in cm.
+9.4
+De omtrek van de cirkel is  59.061892 cm.
+De oppervlakte van de cirkel is  277.59089240000003 vierkante cm.
+'''
